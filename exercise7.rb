de7efaa4e55dea2df9c1587a76419c3c79117526
jpileto <jp.ileto@icloud.com>
2016-10-18 18:26:40 -0400

    doing last problem in exercise 7

---
+++ exercise7.rb
@@ -0,0 +1,22 @@
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+students.each do |k, v| puts "#{k}: #{v}"
+end
+
+students[:cohort4] = 43
+students.each do |k, v| puts "#{k}: #{v}"
+end
+
+students.each do |k, v| puts "#{k}"
+end
+
+students.each do |k, v| puts "#{k}: #{(v * 1.05).to_i}"
+end
+
+students.delete(:cohort2)
+students.each do |k, v| puts "#{k}: #{(v * 1.05).to_i}"
+end
